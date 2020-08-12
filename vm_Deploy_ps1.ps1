2bbebd8337290888693f22dab4c984b4cc6b1359
sabarichida <64587676+sabarichida@users.noreply.github.com>
2020-08-12 14:14:37 +0100

    Delete vm_Deploy_ps1.ps1

--- vm_Deploy_ps1.ps1
+++
@@ -1,4 +0,0 @@
-Test-AzureRmResourceGroupDeployment -ResourceGroupName "CORE_RG_EU" -TemplateFile "F:\aws\Azure\ARM Template\VM_deploy\template.json" -TemplateParameterFile "F:\aws\Azure\ARM Template\VM_deploy\parameters.json"  -Verbose
-
-
-New-AzureRmResourceGroupDeployment -Name "NPS_Machine" -ResourceGroupName "CORE_RG_EU" -TemplateFile "F:\aws\Azure\ARM Template\VM_deploy\template.json" -TemplateParameterFile "F:\aws\Azure\ARM Template\VM_deploy\parameters.json"  -Verbose
